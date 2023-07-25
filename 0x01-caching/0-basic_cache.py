c0225d0b89f045e754345845a43558ea3a236c05
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-25 14:23:15 +0100

    Add Task 1

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,6 +9,10 @@
             - Must assign to the dictionary self.cache_data the item
               value for the key key.
             - If key or item is None, this method should not do anything.
+        - def get(self, key):
+            - Must return the value in self.cache_data linked to key.
+            - If key is None or if the key doesn’t exist in
+              self.cache_data, return None.
 """
 
 BaseCaching = __import__('base_caching').BaseCaching
---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+    Create a class FIFOCache that inherits from
+    BaseCaching and is a caching system:
+
+        - You must use self.cache_data - dictionary from the
+          parent class BaseCaching
+        - You can overload def __init__(self): but don’t
+          forget to call the parent init: super().__init__()
+        - def put(self, key, item):
+            - Must assign to the dictionary self.cache_data
+              the item value for the key key.
+            - If key or item is None, this method should not do anything.
+            - If the number of items in self.cache_data is
+              higher that BaseCaching.MAX_ITEMS:
+            - you must discard the first item put in cache (FIFO algorithm)
+            - you must print DISCARD: with the key discarded and
+              following by a new line
+        - def get(self, key):
+            - Must return the value in self.cache_data linked to key.
+            - If key is None or if the key doesn’t exist in
+              self.cache_data, return None.
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Defines a FIFO cachning system"""
+    def __init__(self):
+        """Initialize an Instance"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Caches the given data using FIFO algorithm"""
+        if key and item:
+            cache = self.cache_data
+            cache[key] = item
+            if len(cache) > self.MAX_ITEMS:
+                trash = list(cache.keys())[0]
+                del cache[trash]
+                print('DISCARD: {}'.format(trash))
+
+    def get(self, key):
+        """Returns the value of key from self.cache_data"""
+        if not self.cache_data.get(key):
+            return None
+        return self.cache_data[key]
