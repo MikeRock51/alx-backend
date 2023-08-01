c12e3b50793a3ca410709a4fff7f5613a9dbc73b
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-01 18:52:21 +0100

    Add Task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+    First you will setup a basic Flask app in 0-app.py.
+    Create a single / route and an index.html template
+    that simply outputs “Welcome to Holberton” as page
+    title (<title>) and “Hello world” as header (<h1>).
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Configuration for the application"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Determines the best language match to use"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def welcome():
+    """Returns the index page"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
