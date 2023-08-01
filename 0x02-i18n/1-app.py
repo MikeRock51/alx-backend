93ba5ae8916c09344b87eeceb9df92856ef0ac5b
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-01 15:31:27 +0100

    Comment config lines

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,12 +8,12 @@
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
-from config import Config
+#from config import Config
 
 
 app = Flask(__name__)
 babel = Babel(app)
-app.config.from_object(Config)
+#app.config.from_object(Config)
 
 
 @app.route('/')
