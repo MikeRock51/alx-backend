e7caef68ebf9e94087206dd51978e8d1dff88f80
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-01 15:30:34 +0100

    Remove config file to confirm

--- 0x02-i18n/config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Contains the app configuration"""
-
-
-class Config:
-    """Configuration for the application"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
