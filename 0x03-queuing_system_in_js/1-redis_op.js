812c7e4f49a68b4b22e5fdd9091270635e39169f
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-29 15:30:27 +0100

    Update task 1

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -2,12 +2,18 @@ import { createClient } from "redis";
 
 const client = createClient();
 
-function setNewSchool(schoolName, value, callback) {
-  client.set(schoolName, value, callback);
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (error, response) => {
+	error && console.log(`Error: ${error}`);
+	response && console.log(`Response: ${response}`);
+  });
 }
 
-function displaySchoolValue(schoolName, callback) {
-  client.get(schoolName, callback);
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (error, value) => {
+	error && console.log(`Error: ${error}`);
+	value && console.log(value);
+  });
 }
 
 client.on("error", (err) =>
@@ -16,13 +22,8 @@ client.on("error", (err) =>
 
 client.on("ready", () => {
   console.log("Redis client connected to the server");
-  displaySchoolValue("Holberton", (error, value) => {
-	console.log(value);
-  });
-  setNewSchool("HolbertonSanFrancisco", "100", (error, response) => {
-	console.log(`Response: ${response}`);
-  });
-  displaySchoolValue("HolbertonSanFrancisco", (error, value) => {
-	console.log(value);
-  });
 });
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
